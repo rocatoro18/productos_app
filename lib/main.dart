9c01aff0255417957b3056861f6a1fbfd2a64c84
Carlos Torres <59813315+rocatoro18@users.noreply.github.com>
2023-02-22 15:34:29 -0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:productos_app/screens/screens.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Productos App',
+      initialRoute: 'login',
+      routes: {
+        'login': (_) => const LoginScreen(),
+        'home': (_) => const HomeScreen()
+      },
+      theme:
+          ThemeData.light().copyWith(scaffoldBackgroundColor: Colors.grey[300]),
+    );
+  }
+}
