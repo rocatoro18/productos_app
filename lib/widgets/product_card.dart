451af169d88d3afce8fedb5349ce70da53fe3aef
Carlos Torres <59813315+rocatoro18@users.noreply.github.com>
2023-02-23 16:11:35 -0700

    product card

--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -11,7 +11,151 @@ class ProductCard extends StatelessWidget {
         margin: const EdgeInsets.only(top: 30, bottom: 50),
         width: double.infinity,
         height: 400,
-        color: Colors.red,
+        decoration: _cardBorders(),
+        child: Stack(
+          alignment: Alignment.bottomLeft,
+          children: [
+            _BackgroundImage(),
+            _ProductDetails(),
+            Positioned(top: 0, right: 0, child: _PriceTag()),
+            // TODO: Mostrar de manera condicional
+            Positioned(top: 0, left: 0, child: _NotAvailable())
+          ],
+        ),
+      ),
+    );
+  }
+
+  BoxDecoration _cardBorders() => BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(25),
+          boxShadow: const [
+            BoxShadow(
+                color: Colors.black12, offset: Offset(0, 7), blurRadius: 10)
+          ]);
+}
+
+class _NotAvailable extends StatelessWidget {
+  const _NotAvailable({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: FittedBox(
+        fit: BoxFit.contain,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Text(
+            'No Disponible',
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+        ),
+      ),
+      width: 100,
+      height: 70,
+      decoration: BoxDecoration(
+          color: Colors.yellow[800],
+          borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(25), bottomRight: Radius.circular(25))),
+    );
+  }
+}
+
+class _PriceTag extends StatelessWidget {
+  const _PriceTag({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: 100,
+      height: 70,
+      decoration: const BoxDecoration(
+          color: Colors.indigo,
+          borderRadius: BorderRadius.only(
+              bottomLeft: Radius.circular(25), topRight: Radius.circular(25))),
+      alignment: Alignment.center,
+      child: FittedBox(
+        fit: BoxFit.contain,
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: const Text(
+            '\$499.99',
+            style: TextStyle(color: Colors.white, fontSize: 20),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class _ProductDetails extends StatelessWidget {
+  const _ProductDetails({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(right: 60),
+      child: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+        width: double.infinity,
+        height: 70,
+        decoration: _buildBoxDecoration(),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: const [
+            Text(
+              'Xbox Series X',
+              style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+            Text(
+              'Id del disco duro',
+              style: TextStyle(
+                fontSize: 15,
+                color: Colors.white,
+              ),
+              maxLines: 1,
+              overflow: TextOverflow.ellipsis,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  BoxDecoration _buildBoxDecoration() => BoxDecoration(
+      color: Colors.indigo,
+      borderRadius: BorderRadius.only(
+          bottomLeft: Radius.circular(25), topRight: Radius.circular(25)));
+}
+
+class _BackgroundImage extends StatelessWidget {
+  const _BackgroundImage({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(25),
+      child: Container(
+        width: double.infinity,
+        height: 400,
+        child: const FadeInImage(
+          placeholder: AssetImage('assets/jar-loading.gif'),
+          image: NetworkImage('https://via.placeholder.com/400x300/f6f6f6'),
+          fit: BoxFit.cover,
+        ),
       ),
     );
   }
