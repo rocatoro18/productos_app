697a5a198aa36d3855ffc4799f2ba87932ed5dda
Carlos Torres <59813315+rocatoro18@users.noreply.github.com>
2023-02-23 10:33:17 -0700

    Inputs styles & validation form

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:productos_app/ui/input_decorations.dart';
 import 'package:productos_app/widgets/widgets.dart';
 
 class LoginScreen extends StatelessWidget {
@@ -36,6 +37,9 @@ class LoginScreen extends StatelessWidget {
                 'Crear una nueva cuenta',
                 style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
               ),
+              const SizedBox(
+                height: 30,
+              ),
             ],
           ),
         ),
@@ -52,27 +56,64 @@ class _LoginForm extends StatelessWidget {
     return Container(
       child: Form(
           // TODO: Mantener la referencia al key
+          autovalidateMode: AutovalidateMode.onUserInteraction,
           child: Column(
-        children: [
-          TextFormField(
-            autocorrect: false,
-            keyboardType: TextInputType.emailAddress,
-            decoration: const InputDecoration(
-                enabledBorder: UnderlineInputBorder(
-                  borderSide: BorderSide(color: Colors.deepPurple),
-                ),
-                focusedBorder: UnderlineInputBorder(
-                    borderSide: BorderSide(color: Colors.deepPurple, width: 2)),
-                hintText: 'john.doe@gmail.com',
-                labelText: 'Correo electrónico',
-                labelStyle: TextStyle(color: Colors.grey),
-                prefixIcon: Icon(
-                  Icons.alternate_email,
+            children: [
+              TextFormField(
+                autocorrect: false,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecorations.authInputDecoration(
+                    hintText: 'john.doe@gmail.com',
+                    labelText: 'Correo electrónico',
+                    prefixIcon: Icons.alternate_email),
+                validator: (value) {
+                  String pattern =
+                      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+                  RegExp regExp = new RegExp(pattern);
+                  return regExp.hasMatch(value ?? '')
+                      ? null
+                      : 'Correo electrónico no válido';
+                },
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              TextFormField(
+                autocorrect: false,
+                obscureText: true,
+                keyboardType: TextInputType.emailAddress,
+                decoration: InputDecorations.authInputDecoration(
+                    hintText: '******',
+                    labelText: 'Contraseña',
+                    prefixIcon: Icons.lock),
+                validator: (value) {
+                  return (value != null && value.length >= 6)
+                      ? null
+                      : 'La contraseña debe de ser de 6 caracteres o mas';
+                },
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              MaterialButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  disabledColor: Colors.grey,
+                  elevation: 0,
                   color: Colors.deepPurple,
-                )),
-          )
-        ],
-      )),
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 80, vertical: 15),
+                    child: const Text(
+                      'Ingresar',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  onPressed: () {
+                    // TODO: Login Form
+                  })
+            ],
+          )),
     );
   }
 }
